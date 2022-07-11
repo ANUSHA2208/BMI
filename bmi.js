be6b92655b2525ca76ffab6f494d9514475ce74a
ANUSHA PATIL <104024116+ANUSHA2208@users.noreply.github.com>
2022-07-11 18:24:39 +0530

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,7 +1,7 @@
       
 $(".try").click(function()
 {
-    window.location.href = "http://127.0.0.1:5500/bmi.html";
+    window.location.href = "https://anusha2208.github.io/BMI/";
 });
 function Calculate()
 {
